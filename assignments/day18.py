8471ad473317f53e3f87f7a27404737438da8360
sachin rana <24794199+sachin141097@users.noreply.github.com>
2018-12-11 23:02:25 +0530

    Delete day18.py

--- assignments/day18.py
+++
@@ -1,7 +0,0 @@
-#Print unique values in dictionary
-L = [{"V":"S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII":"S005"}, {"V":"S009"},{"VIII":"S007"}]
-s = set()
-for key in L:
-   for val in key.values():
-      s.add(val)
-print("Unique values:",s)
